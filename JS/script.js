3d8c8e163532449131a7ab6abda0e4e416f0a8c8
Mateusz Owczarek <owca90@poczta.fm>
2022-09-15 13:00:54 +0200

    change JS

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,19 @@
-console.log("cześć");
+{
+    const welcome = () => {
+        console.log("Witajcie w mojej wizytówce,zapraszam do przejrzenia jej.");
+    }
 
-let button = document.querySelector(".button");
-let main = document.querySelector(".article");
-let nextName = document.querySelector(".nextName");
+    const OnChangeBacgroundClic = () => {
+        main.classList.toggle("article__change-js");
+        nextName.innerText = main.classList.contains("article__change-js") ? "szare" : "białe"
+    };
 
-button.addEventListener("click", () => {
-    main.classList.toggle("article__change-js");
-    nextName.innerText = main.classList.contains("article__change-js") ? "szare" : "białe"
-});
+    welcome();
+    
+    let button = document.querySelector(".button")
+    let main = document.querySelector(".article")
+    let nextName = document.querySelector(".nextName")
+
+    button.addEventListener("click", OnChangeBacgroundClic);
+}
 
